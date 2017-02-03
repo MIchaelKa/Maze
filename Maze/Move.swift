8ae49d4ac804295330473c40973b9b14ae126c2f
Michael Kalinin <michael.kalinin.nn@gmail.com>
2017-02-03 17:28:04 +0300

    Changes for Move node

--- Maze/Move.swift
+++ Maze/Move.swift
@@ -11,39 +11,57 @@ import SpriteKit
 
 class Move: SKShapeNode {
     
+    let cornerRadius: CGFloat = 2.0
+    
     var cell: Cell
     var depth: Int
+    
+    let innerRectNode: SKShapeNode
     
     init(cell: Cell, depth: Int) {
         
         self.cell = cell
         self.depth = depth
+        
+        let innerRectSize = 5.0 * CGFloat(depth)
+        
+        self.innerRectNode = SKShapeNode(rectOf: CGSize(width: innerRectSize, height: innerRectSize), cornerRadius: cornerRadius)
+        self.innerRectNode.fillColor = SKColor.white
+        
         super.init()
         
-        let offset: CGFloat = 10.0 + CGFloat(depth) * 2
-        let size = UI.wallLength - 2 * offset
+        path = path(value: 0)
         
-        
-        let rect = CGRect(origin: CGPoint(x: -size / 2.0, y: -size / 2.0),
-                          size: CGSize(width: size, height: size))
-        
-        path = CGPath(rect: rect, transform: nil)
         strokeColor = SKColor.darkGray
+        
+        //fillColor = SKColor.darkGray
+        //addChild(innerRectNode)
     }
     
     func updateDepth(value: CGFloat) {
+        path = path(value: value)
+        innerRectNode.path = innerPath(value: value)
+    }
+    
+    func path(value: CGFloat) -> CGPath {
         
         let offset: CGFloat = 10.0 + (CGFloat(depth) + value) * 2
         let size = UI.wallLength - 2 * offset
         
-        
         let rect = CGRect(origin: CGPoint(x: -size / 2.0, y: -size / 2.0),
                           size: CGSize(width: size, height: size))
-        path = CGPath(rect: rect, transform: nil)
+        
+        return CGPath(roundedRect: rect,
+                      cornerWidth: cornerRadius,
+                      cornerHeight: cornerRadius,
+                      transform: nil)
     }
     
-    func path(value: CGFloat) {
-        
+    func innerPath(value: CGFloat) -> CGPath {
+        let innerRectSize = 5.0 * (CGFloat(depth) + value)
+        let innerRect = CGRect(origin: CGPoint(x: -innerRectSize / 2.0, y: -innerRectSize / 2.0),
+                               size: CGSize(width: innerRectSize, height: innerRectSize))
+        return CGPath(rect: innerRect, transform: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
